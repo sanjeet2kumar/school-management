b92a3a0c38ed61c969e08a404aaa42b657c41ad9
Sanjeet Kumar <sanjeet111gupta@gmail.com>
2025-11-30 12:28:16 +0530

    swagger config added

---
+++ src/main/java/com/school/school_management/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+//package com.school.school_management.config;
+//
+//import io.swagger.v3.oas.models.info.Contact;
+//import io.swagger.v3.oas.models.info.Info;
+//import io.swagger.v3.oas.models.OpenAPI;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class SwaggerConfig {
+//
+//    @Bean
+//    public OpenAPI schoolManagementAPI() {
+//        return new OpenAPI()
+//                .info(new Info()
+//                        .title("School Management System API")
+//                        .version("1.0")
+//                        .description("API documentation for School Management System")
+//                        .contact(new Contact()
+//                                .name("Your Name")
+//                                .email("your.email@example.com")
+//                        )
+//                );
+//    }
+//}
